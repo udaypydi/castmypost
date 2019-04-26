a403dd26204383b650de7948e6434f4909c0e4b3
udaypydi <udaypydi333@gmail.com>
2019-04-27 04:44:49 +0530

    sidebar component added

--- src/client/components/commons/sidebar/sideBar.component.js
+++ src/client/components/commons/sidebar/sideBar.component.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
@@ -7,13 +8,38 @@ import sideBarStyles from './sideBar.styles';
 
 function SideBar(props) {
 
+  const [sideBarListCollapsed, setSideBarListCollapsed] = useState(true);
+
   function renderSideBarChildren() {
     const { sideBarComponents } = props;
     return [];
   }
+
+  function toggleSideBar() {
+    setSideBarListCollapsed(!sideBarListCollapsed);
+  }
   
   return (
-    <div css={sideBarStyles.container}>
+    <div css={css`${sideBarStyles.container} ${!sideBarListCollapsed ? sideBarStyles.expandedContainer : ''}`}>
+      {sideBarListCollapsed ? (
+         <Icon 
+         name="list" 
+         color="#fff" 
+         css={sideBarStyles.icon} 
+         onClick={toggleSideBar}
+       />
+      ) : 
+      (
+        <React.Fragment>
+          <span>CastMyPost</span>
+          <Icon
+            name="close"
+            css={sideBarStyles.icon}
+            onClick={toggleSideBar}
+          /> 
+        </React.Fragment>  
+      )
+      }
       {renderSideBarChildren()}
     </div>
   )
